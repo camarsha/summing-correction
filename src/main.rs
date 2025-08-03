40138cd66d896a7089fea63e0e46b1e041c90f66
Caleb <marshallc@ohio.edu>
2025-08-03 15:54:21 -0400

    Giving up on tui, finished cli interface.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-// mod draw_levels;
+mod draw_levels;
 mod efficiency;
-// mod interpreter;
 mod level_info;
 mod read_levels;
 mod sum_correction;
 use clap::Parser;
 use indicatif::ProgressBar;
-
+use level_info::Observation;
+use rgsl::MatrixF64;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, arg_required_else_help = true)]
 /// Correct gamma ray intensities based on the summing correction
 /// formalism of Semkov.
 struct Args {
     /// Input file with branching level, branching ratios, and observed values.
-    input: Option<String>,
+    input: String,
 
     /// Path to the two column peak efficiency file.
     #[arg(short, long)]
@@ -24,8 +26,85 @@ struct Args {
     #[arg(short, long)]
     total_eff_file: Option<String>,
 
+    /// Number of Monte-Carlo samples to run.
     #[arg(short, long, default_value_t = 10000)]
     samples: i64,
+
+    /// Output file
+    #[arg(short, long)]
+    output: Option<String>,
+
+    /// Output formatted for humans
+    #[arg(short, long, default_value_t = false)]
+    human_readable: bool,
+}
+
+fn print_function(
+    obs: &mut [Observation],
+    energy_matrix: &MatrixF64,
+    in_file: &str,
+    for_humans: bool,
+) {
+    if for_humans {
+        for o in obs.iter_mut() {
+            match o.corrected_value() {
+                Ok((m, std)) => println!(
+                    "E𝛾 = {0:<10.2} | Observed = {1:<7.1} ± {2:<5.1} | Corrected = {m:<7.1} ± {std:<5.1}",
+                    energy_matrix.get(o.from, o.to),
+                    o.counts,
+                    o.dcounts
+                ),
+                Err(()) => eprintln!(
+                    "Observed transition from {} to {} was not defined in the B-Values section of {}, skipping!",
+                    o.from, o.to, in_file
+                ),
+            };
+        }
+    } else {
+        println!("Eg,counts,dcounts,corrected,dcorrected");
+        for o in obs.iter_mut() {
+            match o.corrected_value() {
+                Ok((m, std)) => println!(
+                    "{0:.2},{1:.3},{2:.3},{m:.3},{std:.3}",
+                    energy_matrix.get(o.from, o.to),
+                    o.counts,
+                    o.dcounts
+                ),
+                Err(()) => eprintln!(
+                    "Observed transition from {} to {} was not defined in the B-Values section of {}, skipping!",
+                    o.from, o.to, in_file
+                ),
+            };
+        }
+    }
+}
+
+fn write_output(obs: &mut [Observation], energy_matrix: &MatrixF64, in_file: &str, out_file: &str) {
+    let output = File::create(out_file).expect("Failed to create output file!");
+    let mut buf_writer = BufWriter::new(output);
+    let _ = buf_writer.write("Eg,counts,dcounts,corrected,dcorrected\n".as_bytes());
+    for o in obs.iter_mut() {
+        match o.corrected_value() {
+            Ok((m, std)) => buf_writer
+                .write(
+                    format!(
+                        "{0:.2},{1:.3},{2:.3},{m:.3},{std:.3}\n",
+                        energy_matrix.get(o.from, o.to),
+                        o.counts,
+                        o.dcounts
+                    )
+                    .as_bytes(),
+                )
+                .expect("Write failed!"),
+            Err(()) => {
+                eprintln!(
+                    "Observed transition from {} to {} was not defined in the B-Values section of {}, skipping!",
+                    o.from, o.to, in_file
+                );
+                0
+            }
+        };
+    }
 }
 
 fn main() {
@@ -35,15 +114,7 @@ fn main() {
     let mut r = rand::rng();
     let args = Args::parse();
 
-    // if args.input.is_none() {
-    //     interpreter::read_loop().unwrap();
-    //     return;
-    // }
-
-    let in_file = match args.input {
-        Some(p) => p,
-        None => "".to_string(),
-    };
+    let in_file = args.input;
 
     let peak_file = match args.peak_eff_file {
         Some(p) => p,
@@ -86,19 +157,11 @@ fn main() {
 
     let energy_matrix = sum_correction::make_tranition_energies(&branches, &levels);
 
-    println!("E𝛾,counts,dcounts,corrected,dcorrected");
-    for o in obs.iter_mut() {
-        match o.corrected_value() {
-            Ok((m, std)) => println!(
-                "{0:.2},{1:.3},{2:.3},{m:.3},{std:.3}",
-                energy_matrix.get(o.from, o.to),
-                o.counts,
-                o.dcounts
-            ),
-            Err(()) => eprintln!(
-                "Observed transition from {} to {} was not defined in the B-Values section of {}, skipping!",
-                o.from, o.to, in_file
-            ),
-        };
+    if args.output.is_none() {
+        print_function(&mut obs, &energy_matrix, &in_file, args.human_readable);
+    } else {
+        if let Some(out_file) = args.output {
+            write_output(&mut obs, &energy_matrix, &in_file, &out_file);
+        }
     }
 }
